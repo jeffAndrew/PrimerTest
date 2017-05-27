a18286ce49e6e599f0f327f0fe822218e0c243ab
JeffAndrew <tajisunse@hotmail.com>
2017-05-27 13:40:02 -0400

    cambiando de la pantalla tabla al centro

---
+++ app/src/main/java/aejg/primertest/ScrollV.java
@@ -0,0 +1,15 @@
+package aejg.primertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ScrollV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_v);
+
+    }
+}
--- app/src/main/java/aejg/primertest/grid.java
+++ app/src/main/java/aejg/primertest/grid.java
@@ -1,8 +1,11 @@
 package aejg.primertest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -14,6 +17,14 @@ public class grid extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid);
+        Button btnScroll = (Button)findViewById(R.id.btnScroll);
+        btnScroll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(grid.this,ScrollV.class);
+                startActivity(intent);
+            }
+        });
 
         GridView gridView = (GridView)findViewById(R.id.gridv);
 
